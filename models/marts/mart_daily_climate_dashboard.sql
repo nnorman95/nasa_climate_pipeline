06d8b32fba6977ba8fc98670483d96fa370244b4
NORMAN <292279233+nnorman95@users.noreply.github.com>
2026-06-27 23:28:28 +0400

    Add dbt daily climate mart model

---
+++ models/marts/mart_daily_climate_dashboard.sql
@@ -0,0 +1,21 @@
+SELECT
+    f.location_id,
+    d.city,
+    d.country,
+    d.latitude,
+    d.longitude,
+    f.weather_date,
+    f.temperature_avg_c,
+    f.temperature_max_c,
+    f.temperature_min_c,
+    f.temperature_max_c - f.temperature_min_c AS temperature_range_c,
+    f.relative_humidity_pct,
+    f.wind_speed_m_s,
+    f.precipitation_mm,
+    f.solar_radiation_kwh_m2_day,
+    f.precipitation_mm > 0 AS is_rainy_day,
+    f.relative_humidity_pct >= 80 AS is_high_humidity_day,
+    f.loaded_at
+FROM {{ ref('fact_daily_weather') }} AS f
+JOIN {{ ref('dim_location') }} AS d
+    ON f.location_id = d.location_id
